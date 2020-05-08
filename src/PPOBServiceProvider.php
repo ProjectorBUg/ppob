fb80bd96190544e2c13a622ef95c32b2ad7703f2
Rootpaceusa <64696314+ahmadchenwangxuesi@users.noreply.github.com>
2020-05-08 21:32:23 +0700

    Create PPOBServiceProvider.php

---
+++ src/PPOBServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ProjectorBUg\PPOB;
+
+use Illuminate\Support\ServiceProvider;
+
+class PPOBServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('ppob', function ($app) {
+            return new PPOBManager($app);
+        });
+    }
+
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/ppob.php' => config_path('ppob.php')
+        ], 'config');
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['ppob'];
+    }
+}
